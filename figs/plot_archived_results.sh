762750b3214b1448becf0d9fa69bc24d749447de
Muhammad Awad <mawad@ucdavis.edu>
2021-11-22 22:19:21 -0800

    Change axis minimum

--- figs/plot_archived_results.sh
+++ figs/plot_archived_results.sh
@@ -2,7 +2,7 @@ mkdir -p ./arxiv/Quadro-GV100
 mkdir -p ./arxiv/NVIDIA-TITAN-V
 mkdir -p ./arxiv/NVIDIA-TITAN-Xp
 mkdir -p ./arxiv/Tesla-V100-DGXS-32GB
-min_max='-mf 1000 -xf 5000 -mi 0 -xi 1800'
+min_max='-mf 0 -xf 5000 -mi 0 -xi 1800'
 python -W ignore ./plot.py $min_max -d ../results/arxiv/Quadro-GV100/ -od ./arxiv/Quadro-GV100/ &> arxiv/Quadro-GV100/summary.txt
 python -W ignore ./plot.py $min_max -d ../results/arxiv/NVIDIA-TITAN-V/ -od ./arxiv/NVIDIA-TITAN-V/ &> arxiv/NVIDIA-TITAN-V/summary.txt
 python -W ignore ./plot.py $min_max -d ../results/arxiv/NVIDIA-TITAN-Xp/ -od ./arxiv/NVIDIA-TITAN-Xp/ &> arxiv/NVIDIA-TITAN-Xp/summary.txt
