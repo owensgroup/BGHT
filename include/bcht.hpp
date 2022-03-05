30173530a3ee1bafa62496319812ee693c1d356c
Muhammad Awad <mawad@ucdavis.edu>
2022-03-05 07:57:30 -0800

    Adding initial hash set

--- include/bcht.hpp
+++ include/bcht.hpp
@@ -218,6 +218,190 @@ struct bcht {
   std::size_t num_buckets_;
 };
 
+/**
+ * @brief BCHT BCHT (bucketed cuckoo hash table) is an associative static GPU hash table
+ * that contains key-value pairs with unique keys. The hash table is an open addressing
+ * hash table based on the cuckoo hashing probing scheme (bucketed and using three hash
+ * functions).
+ *
+ * @tparam Key Type for the hash map key
+ * @tparam Hash Unary function object class that defines the hash function. The function
+ * must have an `initialize_hf` specialization to initialize the hash function using a
+ * random number generator
+ * @tparam KeyEqual Binary function object class that compares two keys
+ * @tparam Allocator The allocator to use for allocating GPU device memory
+ * @tparam B Bucket size for the hash table
+ */
+template <class Key,
+          class Hash = bght::universal_hash<Key>,
+          class KeyEqual = bght::equal_to<Key>,
+          cuda::thread_scope Scope = cuda::thread_scope_device,
+          class Allocator = bght::cuda_allocator<char>,
+          int B = 16>
+struct bchs {
+  using value_type = Key;
+  using key_type = Key;
+  using atomic_value_type = cuda::atomic<value_type, Scope>;
+  using allocator_type = Allocator;
+  using hasher = Hash;
+  using size_type = std::size_t;
+
+  using atomic_value_allocator_type =
+      typename std::allocator_traits<Allocator>::rebind_alloc<atomic_value_type>;
+  using pool_allocator_type =
+      typename std::allocator_traits<Allocator>::rebind_alloc<bool>;
+  using size_type_allocator_type =
+      typename std::allocator_traits<Allocator>::rebind_alloc<size_type>;
+
+  static constexpr auto bucket_size = B;
+  using key_equal = KeyEqual;
+
+  /**
+   * @brief Constructs the hash table with the specified capacity and uses the specified
+   * sentinel key and value to define a sentinel pair.
+   *
+   * @param capacity The number of slots to use in the hash table. If the capacity is not
+   * multiple of the bucket size, it will be rounded
+   * @param sentinel_key A reserved sentinel key that defines an empty key
+   * @param allocator The allocator to use for allocating GPU device memory
+   */
+  bchs(std::size_t capacity, Key sentinel_key, Allocator const& allocator = Allocator{});
+  /**
+   * @brief A shallow-copy constructor
+   */
+  bchs(const bchs& other);
+  /**
+   * @brief Move constructor is currently deleted
+   */
+  bchs(bchs&&) = delete;
+  /**
+   * @brief The assignment operator for the bchs is currently deleted
+   */
+  bchs& operator=(const bchs&) = delete;
+  /**
+   * @brief The move assignment operator for the bchs is currently deleted
+   */
+  bchs& operator=(bchs&&) = delete;
+  /**
+   * @brief Destructor that destroys the hash map and deallocate memory if no copies exist
+   */
+  ~bchs();
+
+  /**
+   * @brief Clears the hash map and resets all slots
+   */
+  void clear();
+
+  /**
+   * @brief Host-side API for inserting all keys defined by the input argument iterators.
+   * All keys in the range must be unique and must not exist in the hash table.
+   * @tparam InputIt Device-side iterator that can be converted to `value_type`.
+   * @param first An iterator defining the beginning of the input keys to insert
+   * @param last  An iterator defining the end of the input keys to insert
+   * @param stream  A CUDA stream where the insertion operation will take place
+   * @return A boolean indicating success (true) or failure (false) of the insertion
+   * operation.
+   */
+  template <typename InputIt>
+  bool insert(InputIt first, InputIt last, cudaStream_t stream = 0);
+
+  /**
+   * @brief Host-side API for finding all keys defined by the input argument iterators.
+   * @tparam InputIt  Device-side iterator that can be converted to `key_type`
+   * @tparam OutputIt Device-side iterator that can be converted to `mapped_type`
+   * @param first An iterator defining the beginning of the input keys to find
+   * @param last An iterator defining the end of the input keys to find
+   * @param output_begin An iterator defining the beginning of the output buffer to store
+   * the results into. The size of the buffer must match the number of queries defined by
+   * the input iterators.
+   * @param stream  A CUDA stream where the insertion operation will take place
+   */
+  template <typename InputIt, typename OutputIt>
+  void find(InputIt first, InputIt last, OutputIt output_begin, cudaStream_t stream = 0);
+
+  /**
+   * @brief Device-side cooperative insertion API that inserts a single key into the hash
+   * map.
+   * @tparam tile_type A cooperative group tile with a size that must match the bucket
+   * size of the hash map (i.e., `bucket_size`). It must support the tile-wide intrinsics
+   * `ballot`, `shfl`
+   * @param key A key-value key to insert into the hash map. The key must be the same
+   * for all threads in the  cooperative group tile
+   * @param tile  The cooperative group tile
+   * @return A boolean indicating success (true) or failure (false) of the insertion
+   * operation.
+   */
+  template <typename tile_type>
+  __device__ bool insert(value_type const& key, tile_type const& tile);
+
+  /**
+   * @brief Device-side cooperative find API that finds a single key into the hash
+   * map.
+   * @tparam tile_type A cooperative group tile with a size that must match the bucket
+   * size of the hash map (i.e., `bucket_size`). It must support the tile-wide intrinsics
+   * `ballot`, `shfl`
+   * @param key A key to find in the hash map. The key must be the same
+   * for all threads in the  cooperative group tile
+   * @param tile The cooperative group tile
+   * @return The value of the key if it exists in the map or the `sentinel_value` if the
+   * key does not exist in the hash map
+   */
+  template <typename tile_type>
+  __device__ bool find(key_type const& key, tile_type const& tile);
+
+  /**
+   * @brief Host-side API to randomize the hash functions used for the probing scheme.
+   * This can be used when the hash table construction fails. The hash table must be
+   * cleared after a call to this function.
+   * @tparam RNG A pseudo-random number generator
+   * @param rng An instantiation of the pseudo-random number generator
+   */
+  template <typename RNG>
+  void randomize_hash_functions(RNG& rng);
+
+  /**
+   * @brief Compute the number of elements in the map
+   * @return The number of elements in the map
+   */
+  size_type size(cudaStream_t stream = 0);
+
+ private:
+  template <typename InputIt, typename HashMap>
+  friend __global__ void detail::kernels::tiled_insert_kernel(InputIt, InputIt, HashMap);
+
+  template <typename InputIt, typename OutputIt, typename HashMap>
+  friend __global__ void detail::kernels::tiled_find_kernel(InputIt,
+                                                            InputIt,
+                                                            OutputIt,
+                                                            HashMap);
+
+  template <int BlockSize, typename InputT, typename HashMap>
+  friend __global__ void detail::kernels::count_kernel(const InputT,
+                                                       std::size_t*,
+                                                       HashMap);
+
+  std::size_t capacity_;
+  key_type sentinel_key_{};
+  mapped_type sentinel_value_{};
+  allocator_type allocator_;
+  atomic_value_allocator_type atomic_values_allocator_;
+  pool_allocator_type pool_allocator_;
+  size_type_allocator_type size_type_allocator_;
+
+  atomic_value_type* d_table_{};
+  std::shared_ptr<atomic_value_type> table_;
+
+  bool* d_build_success_;
+  std::shared_ptr<bool> build_success_;
+
+  uint32_t max_cuckoo_chains_;
+
+  Hash hf0_;
+  Hash hf1_;
+  Hash hf2_;
+
+  std::size_t num_buckets_;
+};
 }  // namespace bght
 
 template <typename Key, typename T>
---
+++ test/test_bcht_set.cu
@@ -0,0 +1,117 @@
+﻿/*
+ *   Copyright 2021 The Regents of the University of California, Davis
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/sequence.h>
+#include <bcht.hpp>
+#include <cmd.hpp>
+#include <gpu_timer.hpp>
+#include <limits>
+#include <perf_report.hpp>
+#include <rkg.hpp>
+#include <type_traits>
+
+int main(int argc, char** argv) {
+  using key_type = uint32_t;
+  using value_type = uint32_t;
+
+  auto arguments = std::vector<std::string>(argv, argv + argc);
+  std::size_t num_keys =
+      get_arg_value<std::size_t>(arguments, "num-keys").value_or(16ull);
+  double load_factor = get_arg_value<double>(arguments, "load-factor").value_or(0.9);
+  int device = get_arg_value<int>(arguments, "device").value_or(0);
+
+  std::cout << "num-keys: " << num_keys << '\n';
+  std::cout << "load-factor: " << load_factor << '\n';
+  bght::set_device(device);
+
+  std::size_t capacity = double(num_keys) / load_factor;
+
+  auto invalid_key = std::numeric_limits<key_type>::max();
+  auto invalid_value = std::numeric_limits<value_type>::max();
+
+  using pair_type = bght::pair<key_type, value_type>;
+
+  std::vector<key_type> h_keys;
+  std::vector<key_type> h_values;
+  rkg::generate_uniform_unique_pairs(h_keys, h_values, num_keys);
+
+  thrust::device_vector<key_type> d_keys(num_keys);
+  thrust::device_vector<pair_type> d_pairs(num_keys);
+  thrust::device_vector<key_type> d_queries(num_keys);
+  thrust::device_vector<value_type> d_results(num_keys);
+
+  d_keys = h_keys;
+
+  // assign values
+  auto to_pair = [] __host__ __device__(key_type x) { return pair_type{x, x * 10}; };
+  thrust::transform(
+      thrust::device, d_keys.begin(), d_keys.end(), d_pairs.begin(), to_pair);
+
+  // prepare queries
+  d_queries = d_keys;
+
+  bght::bcht<key_type, value_type> test(capacity, invalid_key, invalid_value);
+
+  auto input_start = d_pairs.data().get();
+  auto input_last = input_start + num_keys;
+
+  auto queries_start = d_queries.data().get();
+  auto queries_last = queries_start + num_keys;
+  auto output_start = d_results.data().get();
+
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+  gpu_timer insertion_timer(stream);
+  insertion_timer.start_timer();
+  auto insertion_success = test.insert(input_start, input_last, stream);
+  insertion_timer.stop_timer();
+  auto insertion_s = insertion_timer.get_elapsed_s();
+
+  cuda_try(cudaStreamSynchronize(stream));
+
+  gpu_timer find_timer(stream);
+  find_timer.start_timer();
+  test.find(queries_start, queries_last, output_start, stream);
+  find_timer.stop_timer();
+  auto find_s = find_timer.get_elapsed_s();
+
+  cuda_try(cudaDeviceSynchronize());
+
+  // Comoute stats
+  if (!insertion_success) {
+    std::cout << "Insertion failed\n";
+    std::terminate();
+  }
+  std_cout_perf_report(insertion_s, find_s, num_keys, num_keys);
+
+  // validation
+  thrust::host_vector<value_type> h_results = d_results;
+  thrust::host_vector<value_type> h_queries = d_queries;
+  for (std::size_t i = 0; i < num_keys; i++) {
+    auto key = h_queries[i];
+    auto expected_pair = to_pair(key);
+    auto found_result = h_results[i];
+    if (expected_pair.second != found_result) {
+      std::cout << "Error: expected: " << expected_pair.second;
+      std::cout << ", found: " << found_result << '\n';
+      return;
+    }
+  }
+  std::cout << "Success\n";
+}
