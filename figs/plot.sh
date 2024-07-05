c7e3540fe639b73e4916007ae8d004604421564a
maawad <muhammad.a.awad@gmail.com>
2024-07-05 16:23:06 -0700

    Add CHT results

--- figs/plot.sh
+++ figs/plot.sh
@@ -1,6 +1,6 @@
 gpu="Radeon-RX-7900-XT"
 mkdir -p $gpu
-python -W ignore ./plot.py -d  ../results/$gpu/ -od ./$gpu &> $gpu/$gpu.txt
+python -W ignore ./plot.py -d  ../results/$gpu/ -od ./$gpu 2>&1 | tee "$gpu/$gpu.txt"
 
 # python -W ignore ./plot.py -d  ../results/NVIDIA-TITAN-V/ -od ./NVIDIA-TITAN-V
 # python -W ignore ./plot.py -d  ../build/results/NVIDIA-A100-PCIE-40GB/ -od ./NVIDIA-A100-PCIE-40GB &> NVIDIA-A100-PCIE-40GB.txt
